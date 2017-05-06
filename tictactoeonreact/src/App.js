c456bdd0e7c118b15ac7566fc950e3f80f67b319
Zbynek Ruzicka <zruzicka@email.cz>
2017-05-06 19:14:19 +0200

    (TicTacToeOnReact) Implemented check for the winner and game status.

--- tictactoeonreact/src/App.js
+++ tictactoeonreact/src/App.js
@@ -12,21 +12,54 @@ class App extends Component {
       PLAYER_TWO_SYMBOL : '0',
       currentTurn : '',
       winningCombos : [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]],
-      board : ["", "", "", "", "", "", "", "", ""]
+      board : ["", "", "", "", "", "", "", "", ""],
+      turn : 1,
+      gameOver : false  
     }
   }
 
   handleClick(index) {
-    if(this.state.board[index] != "") {
+    if(this.state.board[index] !== "" || this.state.gameOver) {
         return; // No update needed.
     }
-    var currentTurn = this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.PLAYER_TWO_SYMBOL : this.state.PLAYER_ONE_SYMBOL;
+    var currentTurn = this.getNextSymbol();
     var board = this.state.board;
     board[index] = currentTurn;
     this.setState({
       currentTurn : currentTurn,
       board: board
     });
+
+    this.setState({
+      turn: this.state.turn + 1
+    });
+    console.log("turn: " + this.state.turn);
+
+    var symbols = this.state.board;
+    console.log("symbols: " + symbols);
+
+    var winningCombo = this.checkForWinner(symbols);
+    if (winningCombo) {
+      this.setState({
+        gameOver: true
+      });
+      console.log("(turn:"+this.state.turn+"; winningCombo:"+winningCombo+"; symbol:"+symbols[winningCombo[0]]+")");
+      alert("Symbol '"+symbols[winningCombo[0]]+"' won!");
+    }
+  }
+
+  getNextSymbol() {
+    return this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.PLAYER_TWO_SYMBOL : this.state.PLAYER_ONE_SYMBOL;
+  }
+
+  checkForWinner(symbols) {
+    return this.state.winningCombos.find(function(combo) {
+        if(symbols[combo[0]] == symbols[combo[1]] && symbols[combo[1]] == symbols[combo[2]]) {
+          return symbols[combo[0]];
+        } else {
+          return false;
+        }
+      });
   }
 
   render() {
@@ -45,10 +78,17 @@ class App extends Component {
       </div>
       </center>
 
+      <p>Game status: {this.getGameStatus()}</p>
+
       </div>
     )
 
   }
+
+  getGameStatus() {
+    return !this.state.gameOver ? "running" : "finished";
+  }
+
 }
 
 
